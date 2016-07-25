f2db13483321171fa37191cdda063818b676bd5b
sunnyjoe <jiaoqingsunny@gmail.com>
2016-07-25 18:33:32 +0800

    add score

--- ColourMemory/ColourMemory/MainViewController.swift
+++ ColourMemory/ColourMemory/MainViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    let scoreLabel = UILabel()
+    let reminderLabel = UILabel()
+    
+    private var _currentScore = 0
+    var currentScore : Int {
+        set {
+            _currentScore = newValue
+            scoreLabel.withText("Score: \(_currentScore)")
+        }
+        get { return _currentScore }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.black24()
+        
+        scoreLabel.frame = CGRectMake(view.frame.size.width / 2 - 80, 20, 160, 44)
+        view.addSubview(scoreLabel)
+        scoreLabel.withTextColor(UIColor.whiteColor()).textCentered()
+        currentScore = 0
         
         let height = (view.frame.size.width - 15) * 190 / 152 + 15
         let spacing = (view.frame.size.height - height - 64) / 2
@@ -29,17 +45,39 @@ class MainViewController: UIViewController {
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
+    
+    func showReminderLabel(getScore : Int){
+        reminderLabel.withTextColor(UIColor.whiteColor()).textCentered().withFontHeleticaMedium(18)
+        if getScore >= 0 {
+            reminderLabel.withText("+\(getScore)")
+        }else{
+            reminderLabel.withText("\(getScore)")
+        }
+        view.addSubview(reminderLabel)
+        reminderLabel.alpha = 1
+        reminderLabel.frame = CGRectMake(view.frame.size.width / 2 - 50, 300, 100, 44)
+        let scoreFrame = self.scoreLabel.frame
+        UIView.animateWithDuration(1, animations: {
+            self.reminderLabel.alpha = 1
+            self.reminderLabel.frame = CGRectMake(scoreFrame.origin.x, scoreFrame.origin.y + 25, scoreFrame.size.width, scoreFrame.size.height)
+            }, completion: {(Bool) -> Void in
+                self.reminderLabel.removeFromSuperview()
+                self.currentScore += getScore
+            })
+        }
 }
 
 extension MainViewController: ColourCollectionViewDelegate {
     func colourCollectionViewMemoryFailed(colourView: ColourCollectionView) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.8 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            self.showReminderLabel(-1)
             colourView.recoverFlippedCard()
         }
     }
     
     func colourCollectionViewMemorySuccess(colourView: ColourCollectionView) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * Int64(NSEC_PER_SEC)), dispatch_get_main_queue()) {
+            self.showReminderLabel(2)
             colourView.destroyFlippedCard()
         }
     }
